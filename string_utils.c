688114d597b98148b34c651b9ddd6d2b4d44fd85
Hamza Rochd <92557439+HaRo97@users.noreply.github.com>
2024-04-02 02:52:01 +0000

    fractol + bonus fractals - v1

---
+++ string_utils.c
@@ -0,0 +1,71 @@
+#include "fractol.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n && s1[i] && s2[i])
+	{
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		i++;
+	}
+	// if (i < n)
+	// 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
+
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	int	i;
+
+	i = 0;
+	if (!s || fd < 0)
+		return ;
+	while (s[i])
+	{
+		write(fd, &s[i], 1);
+		i++;
+	}
+}
+
+double	ft_atod(const char *str)
+{
+	int		i;
+	double		sign;
+	double result_int;
+	double result_fract;
+	double div;
+
+	i = 0;
+	result_int = 0;
+	result_fract = 0;
+	div = 10;
+	sign = 1;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result_int = 10 * result_int + (str[i] - 48);
+		i++;
+	}
+	if(str[i] == '.')
+	{
+		i++;
+		while (str[i] >= '0' && str[i] <= '9')
+		{
+			result_fract = result_fract + ((1 / div) * (str[i] - 48));
+			div *= 10;
+			i++;
+		}
+	}
+	return (sign * (result_int + result_fract));
+}
